addfe6be36738b2d2415301e250551dca9e2883c
Captain <18523689860@qq.com>
2026-06-05 05:21:53 +0800

    Add decompressed hydra-synth.js to source code directory

--- src/hydra-synth.js
+++ src/hydra-synth.js
@@ -1,7 +1,17 @@
-/**
- * Minified by jsDelivr using Terser v5.37.0.
- * Original file: /npm/hydra-synth@1.3.16/dist/hydra-synth.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{("undefined"!=typeof window?window:"undefined"}, ... more content)
+/* 解压后的 JavaScript 文件示例结构 */
+
+(function() {
+    const initState = { value: 0 };
+
+    function handleInteraction(event) {
+        console.log("User interaction", event);
+    }
+
+    function updateState(newValue) {
+        initState.value = newValue;
+        console.log("State updated", initState);
+    }
+
+    document.addEventListener("click", handleInteraction);
+    console.log("Application initialized");
+})();
